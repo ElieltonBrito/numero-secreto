403eab083564d16929ce838d239a4567eb3594a9
Eleilton Brito <tonbaixo1@gmail.com>
2023-11-15 18:12:42 -0300

    deixando o jogo mais dificil de 30 para 50

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("""
-                            
-                            
+
+
                                  NUMERO SECRETO
-                            
+
                 Você tem 5 chances de acertar qual é o numero escondido de 0 a 100
-                
+
                                       BOA SORTE!!!
                 """);
 
-
         int numeroDeTentativas = 0;
-        int numeroAleatorio = new Random().nextInt(30);
+        int numeroAleatorio = new Random().nextInt(50);
         int numero = 0;
-
 
         Scanner ler = new Scanner(System.in);
 
@@ -25,22 +23,22 @@ public class Main {
             System.out.println("Tente acertar o número: ");
             numero = ler.nextInt();
 
-            if(numero < numeroAleatorio){
+            if (numero < numeroAleatorio) {
                 System.out.println("O número digitado é menor que o número escondido");
 
             }
 
-            if(numero > numeroAleatorio){
+            if (numero > numeroAleatorio) {
                 System.out.println("O número digitado é maior que o número escondido");
             }
 
-            if (numero == numeroAleatorio){
+            if (numero == numeroAleatorio) {
                 System.out.println("Você acertou");
                 break;
             }
 
         }
-            if (numero != numeroAleatorio) {
+        if (numero != numeroAleatorio) {
 
             System.out.println("Você perdeu... O numero escondido é: " + numeroAleatorio);
         }
